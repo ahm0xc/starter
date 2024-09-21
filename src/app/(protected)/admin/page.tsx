d6ed687648e94d02ff9c0a0187392e89a113c513
ahm0xc <codingwebsa@gmail.com>
2024-09-21 20:35:31 +0600

    add info-card and transactions-list to admin page

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,12 +1,37 @@
-import { DashboardHeader } from "../_components/dashboard-header";
+import { redirect } from "next/navigation";
 
-export default function AdminPage() {
+import { auth } from "~/lib/auth";
+import { constructMetadata } from "~/utils/base";
+
+import { DashboardHeader } from "../_components/dashboard-header";
+import InfoCard from "./_components/info-card";
+import TransactionsList from "./_components/transactions-list";
+
+export const metadata = constructMetadata({
+  title: "Admin - SaaS Starter",
+  description: "Admin page for only admin management.",
+});
+
+export default async function AdminPage() {
+  const session = await auth();
+  if (session?.user.role !== "ADMIN") redirect("/login");
+
   return (
-    <div>
+    <>
       <DashboardHeader
         heading="Admin Panel"
         text="Access only for users with ADMIN role."
       />
-    </div>
+      <div className="flex flex-col gap-5">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4">
+          <InfoCard />
+          <InfoCard />
+          <InfoCard />
+          <InfoCard />
+        </div>
+        <TransactionsList />
+        <TransactionsList />
+      </div>
+    </>
   );
 }
