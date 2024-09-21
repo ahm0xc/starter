bd81a5173dd81e1f765f32397ef84ab7ab8be771
ahm0xc <codingwebsa@gmail.com>
2024-09-21 16:27:51 +0600

    UI: update colors

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,39 +1,33 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
+import { MoonStars, Sun } from "@phosphor-icons/react";
 import { useTheme } from "next-themes";
 
 import { Button } from "~/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu";
 
 export default function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
+
+  function toggleTheme() {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="outline"
+      size="icon"
+      className="h-9 w-9"
+      onClick={toggleTheme}
+    >
+      {/* <Sun className="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" /> */}
+      {/* <Moon className="absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" /> */}
+      {theme === "light" && <Sun size={17} />}
+      {theme === "dark" && <MoonStars size={17} />}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 }
